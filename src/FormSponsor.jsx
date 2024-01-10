4f904cfa0f04fdb723f3c2560f504aa7a2b032a1
medbendadi <itsbendadi@gmail.com>
2024-01-11 00:23:37 +0100

    add modifications

--- src/FormSponsor.jsx
+++ src/FormSponsor.jsx
@@ -72,7 +72,7 @@ const FormSponsor = () => {
                   <label htmlFor="message" className="block mb-2 font-medium text-gray-900 dark:text-gray-400">Quels sont vos objectifs principaux pour nouer un partenariat avec MAISON&OBJET ? (Promotion de votre marque, accueil de vos clients, lancement d'un nouveau produit...)</label>
   <textarea id="message" rows="4" value={message} onChange={e=> setMessage(e.target.value)} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 focus:border-none focus:outline-none" placeholder="laissez un commentaire..."></textarea>
                   </div>
-                  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Envoyer</button>
                </form>
 
 
